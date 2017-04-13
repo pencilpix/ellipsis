37e41306611436ae68fd1208a54431f951231b72
Mohamed hassan <pencilpix11@gmail.com>
2017-04-13 10:41:18 +0200

    test: should excerpt

--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -17,5 +17,36 @@ describe('Ellipsis', () => {
   it('it will be defined', () => {
     expect($.fn.ellipsis).toBeDefined();
   });
+
+
+  describe('generic', () => {
+
+    let text = `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco
+laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+dolor in reprehenderit in voluptate velit esse cillum dolore eu
+fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+sunt in culpa qui officia deserunt mollit anim id est laborum.`
+
+    beforeEach(() => {
+      $('body').append(`<p id="paragraph" style="width: 200px">${text}</p>`);
+    });
+
+
+    afterEach(() => {
+      $('#paragraph').remove();
+    });
+
+    it('should excerpt the text', () => {
+      $('#paragraph').ellipsis({
+        type: 'lines',
+        count: 4
+      });
+
+
+      expect($('#paragraph').text().length).toBeGreaterThan(text.length);
+    });
+  });
 });
 
